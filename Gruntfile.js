f9d106c494523348ae79453199d59f1defb0a9e1
Toon Verwerft <toonverwerft@gmail.com>
2022-11-15 10:20:42 +0100

    Disable rev

--- Gruntfile.js
+++ Gruntfile.js
@@ -403,7 +403,7 @@ module.exports = function (grunt) {
     'uglify',
     // 'imagemin',
     // 'svgmin',
-    'rev',
+    //'rev', // Currently broken in combination with usemin..
     'usemin',
     'htmlmin'
   ]);
